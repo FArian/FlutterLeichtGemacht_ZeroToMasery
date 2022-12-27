b7c531b724dfd377d8a4c05172ffde3c86b63e44
Farhad Arian <farhad.arian@x-tention.ch>
2022-12-27 17:44:51 +0100

    spätere Zuweisung

--- dart_grundlagen/2_variablen.dart
+++ dart_grundlagen/2_variablen.dart
@@ -1,7 +1,7 @@
 void main() {
   int alter = 32; //Initialisierung
 
-  late int alter_2; // Deklaration // Später zuweisung
+  late int alter_2; // Deklaration // für spätere Zuweisung verwendet man 'late'
 
   alter_2 = 48; //Zuweisung
 
