c09c142c856200993601130df834d79815cf0268
Farhad Arian <farhad.arian@x-tention.ch>
2022-12-28 12:06:57 +0100

    datentype declration

--- dart_grundlagen/13_methoden_mit_rückgabewert.dart
+++ dart_grundlagen/13_methoden_mit_rückgabewert.dart
@@ -1,14 +1,13 @@
-
-void main(){
+void main() {
   print("vor");
-  int ergebnis = addieren(x:10,y:11);
+  int ergebnis = addieren(x: 10, y: 11);
   print(ergebnis);
-  int ergebnis2 = addieren(x:10,y: ergebnis);
+  int ergebnis2 = addieren(x: 10, y: ergebnis);
   print(ergebnis2);
   print("danach");
 }
 
-int addieren({required int x,required int y}){
-  int z = x+y;
+int addieren({required int x, required int y}) {
+  int z = x + y;
   return z;
 }
--- dart_grundlagen/3_datentypen.dart
+++ dart_grundlagen/3_datentypen.dart
@@ -1,13 +1,11 @@
+void main() {
+  int alter = 32; //Iteger
 
-void main(){
-  
-  int alter = 32;    //Iteger
-
-  double meinDouble = 4.4;  // double
+  double meinDouble = 4.4; // double
 
   bool meinBool = false; // bool
 
-  //  ###############################  
+  //  ###############################
 
   String meinString = "Der Ball";
 
@@ -37,9 +35,9 @@ void main(){
 
   // ! Statisch
 
-  int alter1 = 32;    //Iteger
+  int alter1 = 32; //Iteger
 
-  double meinDouble1 = 4.4;  // double
+  double meinDouble1 = 4.4; // double
 
   bool meinBool1 = false; // bool
 
@@ -47,17 +45,36 @@ void main(){
 
   // ! Automatisch
 
-  var alter2 = 32;    //Iteger
+  var alter2 = 32; //Iteger
 
-  var meinDouble2 = 4.4;  // double
+  var meinDouble2 = 4.4; // double
 
   var meinBool2 = false; // bool
 
   var meinString2 = "Der Ball";
 
-  dynamic variable3 ;
+  dynamic variable3;
 
   variable3 = 3;
 
   variable3 = "string";
+
+  int x = 2;
+  double y = 3.2;
+  num z = 2.2;
+  num q = 2;
+  var intType = 3;
+  var doubleType = 4.7;
+  var stringType = ' VAR AS STRING DATENTYPE';
+  var stringType2 = "VAR AS STRING DATENTYPE";
+  var stringType3 = "'VAR AS STRING DATENTYPE";
+  print("INT :$x");
+  print("DOUBLE :$y");
+  print("NUM as INT :$q");
+  print("NUM as Double :$z");
+  print("var as Int :$intType");
+  print("var as Double :$doubleType");
+  print("var as String :$stringType");
+  print("var as String2 :$stringType2");
+  print("var as String2 :$stringType3");
 }
